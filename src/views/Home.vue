5d05561a69b143d1faeb00572c79272dc5fecae8
Stratos-Chen <twinschen55@gmail.com>
2020-08-04 11:54:19 -0700

    added background image and quote to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+        <!-- #header -->
+    <section id="header" style="background-image: url('https://static.tvtropes.org/pmwiki/pub/images/heroes_illust_logo_3.jpg');">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <blockquote>
+              <p>The Heroes who defy the world</p>
+              <small>FeH</small>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="overlay"></div>
+      </section><!-- #header -->
   </div>
 </template>
 
