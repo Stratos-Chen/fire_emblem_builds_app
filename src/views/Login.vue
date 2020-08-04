b0b35276764d068a21e138046b20c0c48773d7cf
Stratos-Chen <twinschen55@gmail.com>
2020-08-04 10:51:31 -0700

    added new redirects for login and logout files

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -41,7 +41,7 @@ export default {
           axios.defaults.headers.common["Authorization"] =
             "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
-          this.$router.push("/");
+          this.$router.push("/users/me");
         })
         .catch(error => {
           this.errors = ["Invalid email or password."];
--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -10,7 +10,7 @@ export default {
   created: function() {
     delete axios.defaults.headers.common["Authorization"];
     localStorage.removeItem("jwt");
-    this.$router.push("/");
+    this.$router.push("/login");
   }
 };
 </script>
