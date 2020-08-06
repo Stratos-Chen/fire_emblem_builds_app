ef28c4fc4d3b7cc74fe4da683f5898318bc93ee3
Stratos-Chen <twinschen55@gmail.com>
2020-08-06 00:07:11 -0700

    modified user Show file format

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="users-show">
-    <!-- <div id="content" class="site-content">
-        <div id="primary" class="content-area">
-          <main id="main" class="site-main" role="main">
+    <div id="content" class="site-content">
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8">
+                <div class="entry-author-bio">
+                  <!-- <div class="author-bio-avatar">                   
+                  </div> -->
+                  <div class="author-bio-description">
+                    <h2>Username: {{ user.username }}</h2>
+                    <p>Email: {{ user.email }}</p>
 
-            <div class="container">
-              <div class="row">
-                <div class="col-md-8">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <a href="/users/me/edit" class="btn btn-lg btn-danish"
+                          >Update Information</a
+                        >
+                      </div>
 
-                  <div class="entry-author-bio">
-                    <div class="author-bio-avatar">
-                    </div> -->
-    <h2>Username: {{ user.username }}</h2>
-    <p>Email: {{ user.email }}</p>
-    
-    <router-link :to="`/users/me/edit`">Update Information</router-link> <br>
-
-    <button><router-link :to="`/builds`">Build List</router-link></button>
+                      <div class="col-md-12">
+                        <a href="/builds" class="btn btn-lg btn-danish"
+                          >Build List</a
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,14 +43,14 @@ export default {
   data: function() {
     return {
       errors: [],
-      user: {}
+      user: {},
     };
   },
   created: function() {
-    axios.get(`/api/users/me`).then(response => {
+    axios.get(`/api/users/me`).then((response) => {
       console.log(response.data);
       this.user = response.data;
     });
-  }
+  },
 };
 </script>
